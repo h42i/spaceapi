b870d1e41ddf0b1c15bb1015fcea794df0f419e0
danb <daniel@localhost.localdomain>
2013-11-03 17:56:44 +0100

    Added improved open state functionality.

--- spaceapi.rb
+++ spaceapi.rb
@@ -1,16 +1,43 @@
 require 'sinatra'
 require 'json'
 require 'net/http'
-require 'ri_cal'
 
 set :port, 65010
 
-get '/*' do
+get '/' do
   #headers.delete('Cache-Control')
   headers['Access-Contol-Allow-Origin'] = '*'
   headers['Content-Type'] = 'application/json'
   #headers['Cache-Control'] = 'no-cache'
   generate_json
+end
+
+get '/set_open/:argument' do
+  begin
+    open_state = params[:argument]
+
+    if open_state == "true"
+      open_one = File.open("space/state/open.boolean", "w")
+      open_two = File.open("space/open.boolean", "w")
+
+      open_one.write("true")
+      open_two.write("true")
+
+      open_one.close
+      open_two.close
+    else
+      open_one = File.open("space/state/open.boolean", "w")
+      open_two = File.open("space/open.boolean", "w")
+
+      open_one.write("false")
+      open_two.write("false")
+
+      open_one.close
+      open_two.close
+    end
+  rescue
+    # bad.
+  end
 end
 
 def generate_json
@@ -39,52 +66,6 @@ def generate_static(hash, dir)
 end
 
 def generate_non_static(hash)
-  # hasi state
-  if not hash.has_key?('state')
-    hash['state'] = Hash.new
-  end
-  
-  begin
-    url = URI.parse('http://stats.l3kn.de/api/bool')
-    request = Net::HTTP::Get.new(url.path)
-    response = Net::HTTP.start(url.host, url.port) { |http|
-      http.request(request)
-    }
-    
-    hash['state']['open'] = response.body.strip() == "true"
-  rescue
-    hash['state']['open'] = false
-  end
-  
-  hash['open'] = hash['state']['open']
-  
-  # hasi events
- # begin
- #   url = URI.parse('webcal://p06-calendarws.icloud.com/ca/subscribe/1/UBv-TIGJfFoHvGX1Y3IAW_b_RH1l2kaXsN7A1WWNeRCCJBhheEGTp0MqKpds2EJzUaEwhJoFM9iieG9_M3ygAD2RXJWFgSv4Yr6PYPzVUgA')
- #   request = Net::HTTP::Get.new(url.path)
- #   response = Net::HTTP.start(url.host, url.port) { |http|
- #     http.request(request)
- #   }
- #   puts response.body
- #   cals = RiCal.parse_string(response.body.unpack('U*').pack('U*'))
- #   
- #   hash['events'] = []
- #   
- #   cals.each do |cal|
- #     cal.event do |event|
- #       event_hash = Hash.new
- #       
- #       event_hash['name'] = event.description,
- #       event_hash['type'] = "?",
- #       event_hash['timestamp'] = event.dtstart.to_time.to_i,
- #       event_hash['extra'] = "?"
- #       
- #       hash['events'].push(event_hash)
- #     end
- #   end
-  #rescue
-  #  hash['events'] = []
-  #end
 end
 
 def put_key(hash, key, value)
@@ -103,11 +84,19 @@ def put_value_from_file(hash, file)
       value.map!(&:to_i)
     elsif File.extname(file) == '.float' or File.extname(file) == '.floatarray'
       value.map!(&:to_f)
+    elsif File.extname(file) == '.boolean' or File.extname(file) == '.booleanarray'
+      i = 0
+
+      while i < value.length do
+        value[i] = value[i] == "true"
+        i += 1
+      end
     end
     
     is_array = File.extname(file) == '.stringarray' or  \
                File.extname(file) == '.integerarray' or \
-               File.extname(file) == '.floatarray'
+               File.extname(file) == '.floatarray' or \
+               File.extname(file) == '.booleanarray'
     
     if value.length == 1 and not is_array
       put_key(hash, key, value[0])
